58e6a63ac966fc2b9ee4d0fe537c3d673d593304
zkakiya <58466600+zkakiya@users.noreply.github.com>
2022-05-24 03:59:50 +0900

    各種修正点を一度に反映

--- usbhid_joypad_to_dsub15.ino
+++ usbhid_joypad_to_dsub15.ino
@@ -32,31 +32,38 @@ int frame_count;//フレームカウント
 
 //仮想ジョイパッドの状態を管理
 //方向キーの状態（方向キー）
-bool VHatState[4];
+bool VHatState[HANDLE_ARROW_DIRECTION];
 //方向キー以外の状態（方向キー以外）
-bool VButtonState[HANDLE_BUTTON_NUM];
+bool VButtonState[HANDLE_BUTTON_NUM + HANDLE_MACRO_NUM];
+// マクロの活性化フラグ
+bool isMacroActive[HANDLE_MACRO_NUM];
 
 //物理ジョイパッドの状態を管理
 //方向キーの状態（方向キー）
-bool PHatState[4];
+bool PHatState[HANDLE_ARROW_DIRECTION];
 //方向キー以外の状態（方向キー以外）
 bool PButtonState[HANDLE_BUTTON_NUM];
 
 //キーアサインの定義
 struct KeyAssign {
-        //hatスイッチの上下反転フラグ（デフォルトは反転なし→false）
-        bool isHatReverse;
+        //仮想スティックにアサインする物理スイッチを選択する。
+        //0（デフォルト）:hatスイッチ、1:アナログスティック1、2:アナログスティック2
+        int directionSwSelctor;
+        bool isDirectionReverse;//hatスイッチの上下反転フラグ（デフォルトは反転なし→false）
+        uint8_t stick01Assign[HANDLE_ARROW_DIRECTION];//スティック01のキーアサイン
+        uint8_t stick02Assign[HANDLE_ARROW_DIRECTION];//スティック02のキーアサイン
         //hatスイッチ以外のキーアサイン
         uint8_t buttonAssignTable[HANDLE_BUTTON_NUM];
 };
-
-//仮想ジョイパッドの配列一時期億
-struct KeyAssign VGAssign;
+//コントロールキュー（マクロの記述およびピン出力準備に使うデータ型）の定義
+struct controlQueue {
+        bool hatState[HANDLE_ARROW_DIRECTION];
+        bool buttonAssignTable[HANDLE_BUTTON_NUM];
+};
 
 //プリセットの状態を管理する
 int keyPresetNum;
 
-//物理ジョイパッドの状態変化によるイベントを受け、仮想ジョイパッドの状態を変更する
 class MyJoystickEvents : public JoystickEvents {
 public:
         void OnGamePadChanged(const GamePadEventData *evt);
@@ -71,10 +78,21 @@ public:
         void virtualJoypadArrowAssign();
         //仮想ボタン割り当て（方向キー以外）
         void virtualJoypadButtonAssign();
+        //ボタン出力の前処理
+        void executeJoypadState();
+        //仮想ジョイパッドの状態をarduinoのピンに反映させる。
+        void pinStateOutput();
+
 private:
-        uint8_t hatState;//hatスイッチの状態を示す変数
+        int hatState[HANDLE_ARROW_DIRECTION];//hatスイッチの状態を示す変数
+        bool stick01State[HANDLE_ARROW_DIRECTION];//スティック01（DualShock4の場合左スティック）の状態を示す変数
+        bool stick02State[HANDLE_ARROW_DIRECTION];//スティック02（DualShock4の場合左スティック）の状態を示す変数        
         bool joypadButtonState[HANDLE_BUTTON_NUM];//接続されたジョイパッドが現在押されているボタンを示す配列
 };
+
+struct KeyAssign VGAssign;
+struct controlQueue outputBuf;
+
 
 class RotarySwChecker {
 public:
@@ -87,31 +105,29 @@ private:
         byte temp_sw_pos = 1;//ロータリSWの変化を検知するための一時的な記録値
 };
 
-//仮想ジョイパッドの状態をarduinoのピンに反映させる。
-void pinStateOutput() {
-//方向キー割り当て
+void MyJoystickEvents::pinStateOutput(void) {
+        //仮想ジョイパッドの状態をarduinoのピンに出力する。
+        //方向キー割り当て
 
-        VHatState[0] ? digitalWrite(2, LOW) : digitalWrite(2, HIGH);
-        VHatState[1] ? digitalWrite(5, LOW) : digitalWrite(5, HIGH);
-        VHatState[2] ? digitalWrite(3, LOW) : digitalWrite(3, HIGH);
-        VHatState[3] ? digitalWrite(4, LOW) : digitalWrite(4, HIGH);
+        outputBuf.hatState[0] ? digitalWrite(2, LOW) : digitalWrite(2, HIGH);
+        outputBuf.hatState[1] ? digitalWrite(5, LOW) : digitalWrite(5, HIGH);
+        outputBuf.hatState[2] ? digitalWrite(3, LOW) : digitalWrite(3, HIGH);
+        outputBuf.hatState[3] ? digitalWrite(4, LOW) : digitalWrite(4, HIGH);
         //その他ボタン割り当て
-        VButtonState[0] ? digitalWrite(6, LOW) : digitalWrite(6, HIGH);
-        VButtonState[1] ? digitalWrite(7, LOW) : digitalWrite(7, HIGH);
-        VButtonState[2] ? digitalWrite(8, LOW) : digitalWrite(8, HIGH);
-        VButtonState[3] ? digitalWrite(A0, LOW) : digitalWrite(A0, HIGH);
-        VButtonState[4] ? digitalWrite(A1, LOW) : digitalWrite(A1, HIGH);
-        VButtonState[5] ? digitalWrite(A2, LOW) : digitalWrite(A2, HIGH);
-        VButtonState[6] ? digitalWrite(A3, LOW) : digitalWrite(A3, HIGH);
-        VButtonState[7] ? digitalWrite(A4, LOW) : digitalWrite(A4, HIGH);
+        outputBuf.buttonAssignTable[0] ? digitalWrite(6, LOW) : digitalWrite(6, HIGH);
+        outputBuf.buttonAssignTable[1] ? digitalWrite(7, LOW) : digitalWrite(7, HIGH);
+        outputBuf.buttonAssignTable[2] ? digitalWrite(8, LOW) : digitalWrite(8, HIGH);
+        outputBuf.buttonAssignTable[3] ? digitalWrite(A0, LOW) : digitalWrite(A0, HIGH);
+        outputBuf.buttonAssignTable[4] ? digitalWrite(A1, LOW) : digitalWrite(A1, HIGH);
+        outputBuf.buttonAssignTable[5] ? digitalWrite(A2, LOW) : digitalWrite(A2, HIGH);
+        outputBuf.buttonAssignTable[6] ? digitalWrite(A3, LOW) : digitalWrite(A3, HIGH);
+        outputBuf.buttonAssignTable[7] ? digitalWrite(A4, LOW) : digitalWrite(A4, HIGH);
         //A5ピンはロータリースイッチ検出に使用するため、JAMMAキー入力には不使用とする。
         // arduino UNO用（11～13ピン）
-        VButtonState[8] ? digitalWrite(11, LOW) : digitalWrite(11, HIGH);
-        VButtonState[9] ? digitalWrite(12, LOW) : digitalWrite(12, HIGH);
-        VButtonState[10] ? digitalWrite(13, LOW) : digitalWrite(13, HIGH);
+        outputBuf.buttonAssignTable[8] ? digitalWrite(11, LOW) : digitalWrite(11, HIGH);
+        outputBuf.buttonAssignTable[9] ? digitalWrite(12, LOW) : digitalWrite(12, HIGH);
+        outputBuf.buttonAssignTable[10] ? digitalWrite(13, LOW) : digitalWrite(13, HIGH);
 }
-
-
 
 USB Usb;
 USBHub Hub(&Usb);
@@ -157,9 +173,11 @@ void loop() {
         Usb.Task();
 
         Serial.println(frame_count);
+        MyJoyEvents.virtualJoypadButtonAssign();
+        MyJoyEvents.executeJoypadState();
         MyJoyEvents.printActiveVButton();
-        pinStateOutput();
-
+        MyJoyEvents.pinStateOutput();
+        
         //ボトルネックになる可能性があるため、30フレームごとの処理としている
         if(frame_count % 60 == 0)
         {
--- usbhidjoypad_outputmanagement.ino
+++ usbhidjoypad_outputmanagement.ino
@@ -1,12 +1,21 @@
 void MyJoystickEvents::OnGamePadChanged(const GamePadEventData *evt) {
-        //アナログスティック検出用の関数　現在はダミー状態
+        //アナログスティックを閾値でデジタル型に変換
+        stick01State[0] = (evt->Z1 < 65) ? true : false;//↑要素
+        stick01State[1] = (evt->Y > 190) ? true : false;//→要素
+        stick01State[2] = (evt->Z1 > 190) ? true : false;//↓要素
+        stick01State[3] = (evt->Y < 65) ? true : false;//←要素
+        stick02State[0] = (evt->Rz < 65) ? true : false;//↑要素
+        stick02State[1] = (evt->Z2 > 190) ? true : false;//→要素
+        stick02State[2] = (evt->Rz > 190) ? true : false;//↓要素
+        stick02State[3] = (evt->Z2 < 65) ? true : false;//←要素
+        virtualJoypadArrowAssign();
 }
 void MyJoystickEvents::OnHatSwitch(uint8_t hat) {
-        // Serial.print("Hat Switch: ");
-        // PrintHex<uint8_t > (hat, 0x80);
-        // Serial.println("");
-
-        hatState = hat;
+        //hatの状態に合わせてhatStateを定義
+        hatState[0] = (hat == 7||hat == 0||hat == 1) ? true : false;//↑要素
+        hatState[1] = (hat == 1||hat == 2||hat == 3) ? true : false;//→要素
+        hatState[2] = (hat == 3||hat == 4||hat == 5) ? true : false;//↓要素
+        hatState[3] = (hat == 5||hat == 6||hat == 7) ? true : false;//←要素
         virtualJoypadArrowAssign();
 }
 void MyJoystickEvents::OnButtonUp(uint8_t but_id) {
@@ -24,17 +33,24 @@ void MyJoystickEvents::OnButtonDn(uint8_t but_id) {
 void MyJoystickEvents::printActiveVButton (void) {
         Serial.print("preset:");
         Serial.print(keyPresetNum + 1);
-        
         Serial.print(" vHat+vKey/pinState:");
-        for(int i = 0;i < sizeof(VHatState);i++){
-                Serial.print(VHatState[i]);
+        for(int i = 0;i < sizeof(outputBuf.hatState);i++){
+                Serial.print(outputBuf.hatState[i]);
         };
-        for(int i = 0;i < sizeof(VButtonState);i++){
-                Serial.print(VButtonState[i]);
+        for(int i = 0;i < sizeof(outputBuf.buttonAssignTable);i++){
+                Serial.print(outputBuf.buttonAssignTable[i]);
         };
         Serial.print(" pKey:");
         for(int i = 0;i < sizeof(PButtonState);i++){
                 Serial.print(PButtonState[i]);
+        };
+        Serial.print(" S1:");
+        for(int i = 0;i < sizeof(stick01State);i++){
+                Serial.print(stick01State[i]);
+        };
+        Serial.print(" S2:");
+        for(int i = 0;i < sizeof(stick02State);i++){
+                Serial.print(stick02State[i]);
         };
         Serial.println();
 }
@@ -42,12 +58,60 @@ void MyJoystickEvents::virtualJoypadArrowAssign(){
         //仮想ジョイパッドの方向キー状態を定義する関数。
         //hatStateが00~07(今回使ったパッドは真上から時計回り）時の状態に応じて仮想ジョイパッドに状態を割り振る
         
-        //hatスイッチが↑要素に入っていた場合、VHatState[0]を真とする。(入っていない場合は偽）
-        VHatState[0] = (((hatState == 7||hatState == 0||hatState == 1)&&(!VGAssign.isHatReverse))||((hatState == 3||hatState == 4||hatState == 5)&&(VGAssign.isHatReverse))) ? true : false;
-        //hatスイッチが→要素に入っていた場合、VHatState[1]を真とする。(入っていない場合は偽）
-        VHatState[1] = (((hatState == 1||hatState == 2||hatState == 3)&&(!VGAssign.isHatReverse))||((hatState == 5||hatState == 6||hatState == 7)&&(VGAssign.isHatReverse))) ? true : false;
-        //hatスイッチが↓要素に入っていた場合、VHatState[2]を真とする。(入っていない場合は偽）
-        VHatState[2] = (((hatState == 3||hatState == 4||hatState == 5)&&(!VGAssign.isHatReverse))||((hatState == 0||hatState == 1||hatState == 7)&&(VGAssign.isHatReverse))) ? true : false;
-        //hatスイッチが←要素に入っていた場合、VHatState[3]を真とする。(入っていない場合は偽）
-        VHatState[3] = (((hatState == 5||hatState == 6||hatState == 7)&&(!VGAssign.isHatReverse))||((hatState == 1||hatState == 2||hatState == 3)&&(VGAssign.isHatReverse))) ? true : false;
+        if (VGAssign.directionSwSelctor == 1) {
+                //VGAssign.directionSwSelctorが1→スティック1の状態を仮想スティックに割り当てる場合
+                //スティックが↑要素に入っていた場合、VHatState[0]を真とする。(入っていない場合は偽）
+                VHatState[0] = (((stick01State[0])&&(!VGAssign.isDirectionReverse))||((stick01State[2])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //スティックが→要素に入っていた場合、VHatState[1]を真とする。(入っていない場合は偽）
+                VHatState[1] = (((stick01State[1])&&(!VGAssign.isDirectionReverse))||((stick01State[3])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //スティックが↓要素に入っていた場合、VHatState[2]を真とする。(入っていない場合は偽）
+                VHatState[2] = (((stick01State[2])&&(!VGAssign.isDirectionReverse))||((stick01State[0])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //スティックが←要素に入っていた場合、VHatState[3]を真とする。(入っていない場合は偽）
+                VHatState[3] = (((stick01State[3])&&(!VGAssign.isDirectionReverse))||((stick01State[1])&&(VGAssign.isDirectionReverse))) ? true : false;
+                for(int i=0;i < sizeof(VGAssign.stick02Assign);i++){VButtonState[VGAssign.stick02Assign[i]] = stick02State[i] ? true : false;}
+        } else if(VGAssign.directionSwSelctor == 2) {
+                //VGAssign.directionSwSelctorが2→スティック2の状態を仮想スティックに割り当てる場合
+                //スティック↑要素時：VHatState[0]を真
+                VHatState[0] = (((stick02State[0])&&(!VGAssign.isDirectionReverse))||((stick02State[2])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //スティック→要素時：VHatState[1]を真
+                VHatState[1] = (((stick02State[1])&&(!VGAssign.isDirectionReverse))||((stick02State[3])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //スティック↓要素時：VHatState[2]を真
+                VHatState[2] = (((stick02State[2])&&(!VGAssign.isDirectionReverse))||((stick02State[0])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //スティック←要素時：VHatState[3]を真
+                VHatState[3] = (((stick02State[3])&&(!VGAssign.isDirectionReverse))||((stick02State[1])&&(VGAssign.isDirectionReverse))) ? true : false;
+                for(int i=0;i < sizeof(VGAssign.stick01Assign);i++){VButtonState[VGAssign.stick01Assign[i]] = stick01State[i] ? true : false;}
+        } else {
+                //VGAssign.directionSwSelctorが0およびその他→hatスイッチの状態を仮想スティックに割り当てる場合
+                //hat↑要素時：VHatState[0]を真
+                VHatState[0] = (((hatState[0])&&(!VGAssign.isDirectionReverse))||((hatState[2])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //hat→要素時：VHatState[1]を真
+                VHatState[1] = (((hatState[1])&&(!VGAssign.isDirectionReverse))||((hatState[3])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //hat↓要素時：VHatState[2]を真
+                VHatState[2] = (((hatState[2])&&(!VGAssign.isDirectionReverse))||((hatState[0])&&(VGAssign.isDirectionReverse))) ? true : false;
+                //hat←要素時：VHatState[3]を真
+                VHatState[3] = (((hatState[3])&&(!VGAssign.isDirectionReverse))||((hatState[1])&&(VGAssign.isDirectionReverse))) ? true : false;
+                for(int i=0;i < sizeof(VGAssign.stick01Assign);i++){VButtonState[VGAssign.stick01Assign[i]] = stick01State[i] ? true : false;}
+                for(int i=0;i < sizeof(VGAssign.stick02Assign);i++){VButtonState[VGAssign.stick02Assign[i]] = stick02State[i] ? true : false;}
+        }
+}
+void MyJoystickEvents::virtualJoypadButtonAssign(void){
+        //マクロ用仮想ボタン押下中判定時の処理
+        //マクロ用仮想ボタン01
+        if(VButtonState[HANDLE_BUTTON_NUM + 1]){
+                outputBuf.buttonAssignTable[0] = true;
+                outputBuf.buttonAssignTable[1] = true;
+        }
+}
+void MyJoystickEvents::executeJoypadState(void){
+        //ピン出力準備
+        //仮想ジョイパッドの状態とマクロ用のキューを組み合わせる
+        for(int i=0;i < HANDLE_ARROW_DIRECTION;i++){
+                outputBuf.hatState[i] = VHatState[i];
+        }
+        for(int i=0;i < HANDLE_BUTTON_NUM;i++){
+                outputBuf.buttonAssignTable[i] = VButtonState[i];
+        }
+
+        virtualJoypadButtonAssign();
+
 }
